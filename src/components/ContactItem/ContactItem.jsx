08512c329c4170131488b0514c2e0625becd0367
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 19:15:47 +0100

    Refactor ContactItem comp

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,25 +1,23 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class ContactItem extends Component {
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-    deleteHandler: PropTypes.func.isRequired,
-  };
-  render() {
-    const { id, name, number, deleteHandler } = this.props;
+const ContactItem = (props) => {
+  const { id, name, number, deleteHandler } = props;
 
-    return (
-      <li key={id}>
-        {name}: {number}
-        <button type="button" id={id} onClick={deleteHandler}>
-          Delete
-        </button>
-      </li>
-    );
-  }
+  return (
+    <li key={id}>
+      {name}: {number}
+      <button type="button" id={id} onClick={deleteHandler}>
+        Delete
+      </button>
+    </li>
+  );
 }
+
+ContactItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  deleteHandler: PropTypes.func.isRequired,
+};
 
 export default ContactItem;
