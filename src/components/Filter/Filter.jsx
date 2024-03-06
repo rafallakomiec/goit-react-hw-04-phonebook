313b94531567ced4416ebd54f997ce4d3ce6cab4
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 19:18:41 +0100

    Refactor Filter comp

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import Proptypes from 'prop-types';
 
-class Filter extends Component {
-  static propTypes = {
-    changeHandler: Proptypes.func.isRequired,
-    filterVal: Proptypes.string.isRequired,
-  };
-
-  render() {
-    const { changeHandler, filterVal } = this.props;
-    return (
-      <label>
-        Find contacts by name:
-        <input type="text" name="filterInput" onChange={changeHandler} value={filterVal} />
-      </label>
-    );
-  }
+const Filter = (props) => {
+  const { changeHandler, filterVal } = props;
+  return (
+    <label>
+      Find contacts by name:
+      <input type="text" name="filterInput" onChange={changeHandler} value={filterVal} />
+    </label>
+  );
 }
+
+Filter.propTypes = {
+  changeHandler: Proptypes.func.isRequired,
+  filterVal: Proptypes.string.isRequired
+};
 
 export default Filter;
